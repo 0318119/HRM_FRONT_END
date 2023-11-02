86a6820e186ff2a2d70414dfdfbf557f99acb02e
Abdur Rehman <a.rehman.mblk@gmail.com>
2023-11-02 20:17:02 +0400

    rehman

--- src/HrOperations/Download_Parameter_Access.js
+++ src/HrOperations/Download_Parameter_Access.js
@@ -9,41 +9,120 @@ import  { connect } from "react-redux";
 import InfiniteScroll from 'react-infinite-scroll-component';
 import * as DOWNLOAD_ACCESS from '../store/actions/HrOperations/downloadAccess/index'
 import { Avatar, Divider, List, Skeleton } from 'antd';
+import { Transfer } from 'antd';
+import { Checkbox } from 'antd';
+import { message } from 'antd';
+import baseUrl from '../config.json'
+import { set } from "react-hook-form";
 
 
 
 const Parameters_Access = ({ Red_Download_Access, GET_DOWNLOAD_ACCESS_DATA }) => {
 
+  const [messageApi, contextHolder] = message.useMessage();
+  var get_access_token = localStorage.getItem("access_token");
 
-  console.log(Red_Download_Access,'Red_Download_Access')
-
-  
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
-  const loadMoreData = () => {
-    if (loading) {
-      return;
-    }
-    setLoading(true);
-    fetch('https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo')
-      .then((res) => res.json())
-      .then((body) => {
-        setData([...data, ...body.results]);
-        setLoading(false);
-      })
-      .catch(() => {
-        setLoading(false);
-      });
-  };
   useEffect(() => {
-    loadMoreData();
     GET_DOWNLOAD_ACCESS_DATA()
+  },[])
+
+
+
+  const [isAllEmployees, setAllEmployees] = useState([])
+  async function AllEmployees() {
+    await fetch(
+      `${baseUrl.baseUrl}/allemployees/GetEmployeesName`, {
+      method: "GET",
+      headers: { "content-type": "application/json", accessToken: `Bareer ${get_access_token}` },
+    }
+    ).then((response) => {
+      return response.json();
+    }).then(async (response) => {
+      if (response.success) {
+        messageApi.open({
+          type: 'success',
+          content: "Get Data Successfully",
+        });
+        setAllEmployees(response.data)
+        console.log(response.data, 'response.')
+        setTimeout(() => {
+          messageApi.destroy()
+        }, 5000);
+      }
+      else {
+        messageApi.open({
+          type: 'error',
+          content: response?.message || response?.messsage,
+        });
+        setTimeout(() => {
+          messageApi.destroy()
+        }, 5000);
+      }
+    }).catch((error) => {
+      messageApi.open({
+        type: 'error',
+        content: error?.message || error?.messsage,
+      });
+      setTimeout(() => {
+        messageApi.destroy()
+      }, 5000);
+    });
+  }
+
+
+  const [mockData, setMockData] = useState([]);
+  const [targetKeys, setTargetKeys] = useState([]);
+  useEffect(() => {
+    AllEmployees()
+
   }, []);
+
+ 
+const [isCheckedData, setCheckedData] = useState([])
+
+// const [isName,setName] = useState();
+// const [isValue,setValue] = useState();  
+// const chkData = (e) =>  {
+//     if (e.target.checked) {
+//       setCheckedData((prevCheckedData) => [
+//         ...prevCheckedData,
+//         { [e.target.name]: e.target.value },
+        
+        
+//       ]);
+//     } else {
+//       setCheckedData((prevCheckedData) =>
+//         prevCheckedData.filter(
+//           (item) => Object.keys(item)[0] !== e.target.name
+//         )
+//       );
+//     }
+// }
+
+
+const [isSetData,setData] = useState([])
+const [getData,setGetData] = useState([])
+
+const CheckData = (e) => {
+  if (isSetData) {
+    getData.push('isSetData')
+    console.log(getData, ';l;llllS555')
+
+  }
+}
+
+
+
+
+
+
+
   return (
     <>
       <div>
         <Header />
       </div>
+      {contextHolder}
       <div className="container">
         <div className="row">
           <div className="col-lg-12 maringClass">
@@ -59,28 +138,36 @@ const Parameters_Access = ({ Red_Download_Access, GET_DOWNLOAD_ACCESS_DATA }) =>
               <Select
                 label={"Select Expense"}
                 placeholder="Please to Select"
-                options={[
-                  {
-                    value: "jack",
-                    label: "Jack",
-                  },
-                  {
-                    value: "lucy",
-                    label: "Lucy",
-                  },
-                  {
-                    value: "Yiminghe",
-                    label: "yiminghe",
-                  },
-                ]}
+                options={Red_Download_Access?.data?.[0]?.res?.data.map(
+                  (item) => ({
+                    value: item.Parameter_code,
+                    label: item.Parameter_Name,
+                  })
+                )}
               />
               <h4 className="text-dark">Select User</h4>
               <hr />
               <div className="row">
+                {/* <div className="col-12">
+                    <Transfer 
+                        dataSource={isAllEmployees}
+                        listStyle={{
+                          width: 500,
+                          height: 300,
+                        }}
+                      // targetKeys={targetKeys}
+                      // onChange={handleChange}
+                    onChange={(item) => {
+                      console.log(item.Emp_name)
+                    }}
+                      render={(item) => item.Emp_name}
+                      selectAllLabels={['', '']}
+                    />
+                  </div>               */}
                 <div className="col-5">
                    <h6 style={{color:"black",}}>All System User</h6>
                   <div d="scrollableDiv" style={{ height: 400, overflow: 'auto', padding: '0 16px', border: '1px solid rgba(140, 140, 140, 0.35)', }}>
-                    <InfiniteScroll dataLength={data.length} next={loadMoreData} hasMore={data.length < 50}
+                    <InfiniteScroll dataLength={''} next={''} hasMore={'data.length < 50'}
                       loader={
                         <Skeleton  avatar
                           paragraph={{
@@ -93,12 +180,22 @@ const Parameters_Access = ({ Red_Download_Access, GET_DOWNLOAD_ACCESS_DATA }) =>
                       scrollableTarget="scrollableDiv"
                     >
                       <List
-                        dataSource={data}
-                        renderItem={(item) => (
+                        dataSource={isAllEmployees}
+                        renderItem={(item,index) => (
                           <List.Item key={item.email}>
                             <List.Item.Meta
                               // avatar={<Avatar src={item.picture.large} />}
-                              title={<a href="https://ant.design">{item.name.last}</a>}
+                              title={<li value={item.Emp_code}>
+                                {/* <Checkbox.Group options={isAllEmployees} defaultValue={item.Emp_code}  /> */}
+                                <input type="checkbox" value={item.Emp_code} name={item.Emp_name} 
+                                  // onChange={(e) => {chkData(e)}}
+                                  onChange={(e) => setData(e.target.name && e.target.value)}
+                                  />
+                                {console.log(isSetData, "kkkkkkk")}
+                                  {
+                                  }
+                                {item.Emp_name}</li>
+                                }
                               // description={item.email}
                             />
                             {/* <div>Content</div> */}
@@ -110,13 +207,13 @@ const Parameters_Access = ({ Red_Download_Access, GET_DOWNLOAD_ACCESS_DATA }) =>
                   <Button title={'Submit'} />
                 </div>
                 <div className="col-2" style={{ height: 400,  padding: '0 16px',display:"flex", justifyContent:"center", alignItems:"center",flexDirection:"column"}}>
-                  <Button title={'Include'} />
+                  <Button title={'Include'}  />
                   <Button title={'Exclude'} />
                 </div>
                 <div className="col-5">
                   <h6 style={{color:"black"}}>All Selected User</h6>
                   <div d="scrollableDiv" style={{ height: 400, overflow: 'auto', padding: '0 16px', border: '1px solid rgba(140, 140, 140, 0.35)', }}>
-                    <InfiniteScroll dataLength={data.length} next={loadMoreData} hasMore={data.length < 50}
+                    <InfiniteScroll dataLength={''} next={''} hasMore={'data.length < 50'}
                       loader={
                         <Skeleton avatar
                           paragraph={{
@@ -129,13 +226,13 @@ const Parameters_Access = ({ Red_Download_Access, GET_DOWNLOAD_ACCESS_DATA }) =>
                       scrollableTarget="scrollableDiv"
                     >
                       <List
-                        dataSource={data}
+                        dataSource={"data"}
                         renderItem={(item) => (
                           <List.Item key={item.email}>
                             <List.Item.Meta
                               // avatar={<Avatar src={item.picture.large} />}
-                              title={<a href="https://ant.design">{item.name.last}</a>}
-                              // description={item.email}
+                              // title={<a href="https://ant.design">{item.name.last}</a>}
+                              description={item.email}
                             />
                           </List.Item>
                         )}
